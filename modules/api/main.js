ca5141351c15b7dc33edcf4d9316f0127dec930c
glabStrizhkov <glabstrizhkovgit@gmail.com>
2022-04-22 09:26:51 +0300

    pm2-v1-noSudo

---
+++ modules/api/main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { IP } = require('robot-npm-ip-holder');
+require('dotenv').config();
+
+const { SendIP } = require('./src/helpers/sendIP');
+
+const { PORT, IP_HOLDER_URL, MY_PRODUCT_ID } = process.env;
+
+const app = express();
+const ipHolder = new IP(IP_HOLDER_URL, MY_PRODUCT_ID);
+const sendIP = new SendIP(ipHolder);
+sendIP.sendIP();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ urlencoded: true }));
+app.use(express.text({ type: 'application/json' }));
+
+app.get('/health', (req, res) => {
+    res.status(200).send("OK");
+});
+app.use('/api', require('./src/api'));
+
+app.listen(PORT, () => console.info(`API on port ${PORT}`));
+
